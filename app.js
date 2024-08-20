7e6d1ad39fe5139123f41d2fed2fd5fb0f912b8b
Glauk Thaqi <glaukthaqi15@gmail.com>
2024-08-20 17:57:33 +0200

    added receivewebhook

--- app.js
+++ app.js
@@ -7,6 +7,40 @@ app.use(express.json());
 const ASANA_ACCESS_TOKEN = '2/1208103241103229/1208103794237447:e084e90d37786fa8f5b6725a1434b745'; // Replace with your Asana access token
 const ASANA_WORKSPACE_ID = '1208103547163783'; // Replace with your Asana workspace ID
 const PROJECT_ID = '1208103379276834'; // Replace with your Asana project ID
+let secret = "";
+
+// Local endpoint for receiving events
+app.post("/receiveWebhook", (req, res) => {
+  if (req.headers["x-hook-secret"]) {
+    console.log("This is a new webhook");
+    secret = req.headers["x-hook-secret"];
+
+    res.setHeader("X-Hook-Secret", secret);
+    res.sendStatus(200);
+  } else if (req.headers["x-hook-signature"]) {
+    const computedSignature = crypto
+      .createHmac("SHA256", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+
+    if (
+      !crypto.timingSafeEqual(
+        Buffer.from(req.headers["x-hook-signature"]),
+        Buffer.from(computedSignature)
+      )
+    ) {
+      // Fail
+      res.sendStatus(401);
+    } else {
+      // Success
+      res.sendStatus(200);
+      console.log(`Events on ${Date()}:`);
+      console.log(req.body.events);
+    }
+  } else {
+    console.error("Something went wrong!");
+  }
+});
 
 // Endpoint to receive webhook events
 app.post('/webhook', async (req, res) => {
@@ -60,7 +94,7 @@ app.post('/setup-webhook', async (req, res) => {
         const response = await axios.post('https://app.asana.com/api/1.0/webhooks', {
             data: {
                 resource: PROJECT_ID,
-                target: 'https://asana-project-8a2bcae75952.herokuapp.com/webhook', // Replace with your deployed Heroku app URL
+                target: 'https://asana-project.herokuapp.com/webhook', // Replace with your deployed Heroku app URL
                 filters: [
                     { "action": "changed", "resource_type": "task" }
                 ]
